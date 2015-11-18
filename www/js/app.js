2e9b76b15bf57fc4d0bd12d34cea1746e5744a3f
ivan varga <ivan.varga.zg@gmail.com>
2015-11-18 04:00:54 +0100

    UI changes

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,7 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -69,5 +69,5 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/browse');
 });
